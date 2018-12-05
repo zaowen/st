298c7258bc75c54b88c2cb1020d54547732e2553
zaowen <penguinking1991@gmail.com>
2018-12-04 23:06:26 -0700

    Color Changes

--- config.def.h
+++ config.def.h
@@ -111,15 +111,14 @@ static const char *colorname[] = {
 	"#555555",
 };
 
-
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 256;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 8;
 
 /*
  * Default shape of cursor
@@ -128,7 +127,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
